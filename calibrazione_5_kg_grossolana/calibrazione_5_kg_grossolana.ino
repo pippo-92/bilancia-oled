539d38994d205a4676d2f254db2165d066123464
pippo-92 <58010882+pippo-92@users.noreply.github.com>
2020-04-27 23:38:54 +0200

    Add files via upload

---
+++ calibrazione_5_kg_grossolana/calibrazione_5_kg_grossolana.ino
@@ -0,0 +1,29 @@
+/* 
+How to Calibrate Your Scale
+https://www.instructables.com/id/How-to-Interface-HX711-Balance-Module-With-Load-Ce/
+ */
+
+#include "HX711.h"
+
+HX711 bilancia(2, 3);
+
+void setup() {
+  Serial.begin(9600);
+ 
+  bilancia.set_scale();
+  bilancia.tare();
+
+  Serial.println("premi");
+  while (!Serial.available()) {}
+
+  float peso_in_uscita = bilancia.get_units(20);
+  float peso = 120.0f;  
+  float parametro_di_calibrazione = peso_in_uscita / peso;
+  
+  Serial.print("parametro_di_calibrazione: ");
+  Serial.println(parametro_di_calibrazione);                 
+}
+
+void loop() {
+  
+}
