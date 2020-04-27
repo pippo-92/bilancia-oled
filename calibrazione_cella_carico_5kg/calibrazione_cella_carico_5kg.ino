3ca3025abc32198d0cd6bc700008a44d544aac96
pippo-92 <58010882+pippo-92@users.noreply.github.com>
2020-04-27 23:40:31 +0200

    Add files via upload

---
+++ calibrazione_cella_carico_5kg/calibrazione_cella_carico_5kg.ino
@@ -0,0 +1,39 @@
+#include "HX711.h"
+
+HX711 bilancia(2, 3);
+
+float parametro_di_calibrazione = -403; // this calibration factor is adjusted according to my load cell
+float peso;
+
+
+void setup() {
+  Serial.begin(9600);
+  Serial.println("HX711 calibration sketch");
+
+  bilancia.set_scale();
+  bilancia.tare();  
+}
+
+void loop() {
+  bilancia.set_scale(parametro_di_calibrazione); 
+
+  Serial.print("lettura: ");
+  peso = bilancia.get_units(); 
+  if (peso < 0) {
+    peso = 0.00;
+  }
+  Serial.print(peso);
+  Serial.print(" g."); 
+  Serial.print(" parametro_di_calibrazione: ");
+  Serial.print(parametro_di_calibrazione);
+  Serial.println();
+
+  if(Serial.available()) {
+    char par = Serial.read();
+    if (par == 'u') {
+      parametro_di_calibrazione += 1;
+    } else if(par == 'd') {
+      parametro_di_calibrazione -= 1;
+    }
+  }
+}
